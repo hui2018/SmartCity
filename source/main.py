0fb77bb8df6f02d85769c1bb798d25ea250def30
Jack <hui2@pdx.edu>
2019-08-04 00:32:20 -0700

    working on displaying a heat map of all possible direction_id from least to most accurate stops

--- source/main.py
+++ source/main.py
@@ -1,39 +1,54 @@
-import plotly.graph_objects as go
+import csv
+import pandas as pd
+from operator import itemgetter
 
-fig = go.Figure()
+'''load csv and sort the location_id for both mean and medium so we can do do the calculation in a for loop'''
 
-fig.add_trace(go.Box(
-  x = [2, 3, 1, 5],
-  y = [['First', 'First', 'First', 'First'],
-       ["A", "A", "A", "A"]],
-  name = "A",
-  orientation = "h"
-))
+data = pd.read_csv('C:\\Users\\Jack\\Desktop\\DataScience\\Ctran\\data.csv')
+df = data.groupby('ZIPCODE')
 
-fig.add_trace(go.Box(
-  x = [8, 3, 6, 5],
-  y = [['First', 'First', 'First', 'First'],
-       ["B", "B", "B", "B"]],
-  name = "B",
-  orientation = "h"
-))
+counter = 0
+percent_result = 0
+result = []
+index = 0
 
-fig.add_trace(go.Box(
-  x = [2, 3, 2, 5],
-  y = [['Second', 'Second', 'Second', 'Second'],
-       ["C", "C", "C", "C"]],
-  name = "C",
-  orientation = "h"
-))
+for x,y in df:
+    #print(x)
+    for z in range(y.RATE.values.size):
+        if(y.RATE.values[z] > 30):
+            counter = counter + 1
+    cal=counter/(y.RATE.values.size)
+    result.append([index,cal])
+    index = index+1
+    #print(cal)
+    counter = 0
+print (result)
+'''
+de = data.groupby('location_id')['location_distance'].median()
+result_median = de.sort_index(ascending=False)
 
-fig.add_trace(go.Box(
-  x = [7.5, 3, 6, 4],
-  y = [['Second', 'Second', 'Second', 'Second'],
-       ["D", "D", "D", "D"]],
-  name = "D",
-  orientation = "h"
-))
+list = []
+'''
+'''
+the analysis is checking if the mean and medium is greater than 30, if the 
+value is greater than 30 for both then we store it in a 2-d array. 
+We then sort the 2d array by mean and display the location id, average, and median
+'''
+'''
+for x in range(result.size):
+    if result.array[x] > 30:
+        if result_median.array[x] > 30:
+            list.append([result.index[x], result.array[x],result_median.array[x] ])
+list = sorted(list, key=itemgetter(1), reverse=True)
 
-fig.update_layout(title_text="Multi-category axis",)
+for x in list:
+    print(x)
 
-fig.show()
+
+with open('C:\\Users\\Jack\\Desktop\\DataScience\\Ctran\\output.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['location_id','average', 'median'])
+    for x in list:
+        writer.writerow(x)
+file.close()
+'''
