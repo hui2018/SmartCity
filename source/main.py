f7be82371dc183a54d996f2874e03ef523cfa765
Jack <hui2@pdx.edu>
2019-08-04 01:38:01 -0700

    Plotted the graph, trying to figure out how to do a heatmap of which stops are the worst to the best accuracy

--- source/main.py
+++ source/main.py
@@ -1,54 +1,47 @@
 import csv
 import pandas as pd
 from operator import itemgetter
+import matplotlib.pyplot as plt
+import numpy as np
 
 '''load csv and sort the location_id for both mean and medium so we can do do the calculation in a for loop'''
-
-data = pd.read_csv('C:\\Users\\Jack\\Desktop\\DataScience\\Ctran\\data.csv')
-df = data.groupby('ZIPCODE')
+data = pd.read_csv('C:\\Users\\Jack\\Desktop\\DataScience\\Ctran\\data_merge.csv')
+df = data.groupby('location_id')
 
 counter = 0
 percent_result = 0
 result = []
+result_lon = []
+result_lat = []
+result_calculation = []
 index = 0
 
 for x,y in df:
     #print(x)
-    for z in range(y.RATE.values.size):
-        if(y.RATE.values[z] > 30):
+    for z in range(y.location_distance.values.size):
+        if(y.location_distance.values[z] > 30):
             counter = counter + 1
-    cal=counter/(y.RATE.values.size)
-    result.append([index,cal])
+    cal=counter/(y.location_distance.values.size)
+    result.append([y.stop_code.values[0],cal,y.stop_lat.values[0], y.stop_lon.values[0]])
+
+    result_lat.append(y.stop_lat.values[0])
+    result_lon.append(y.stop_lon.values[0])
+    result_calculation.append(cal)
+
+
     index = index+1
-    #print(cal)
     counter = 0
-print (result)
-'''
-de = data.groupby('location_id')['location_distance'].median()
-result_median = de.sort_index(ascending=False)
-
-list = []
-'''
-'''
-the analysis is checking if the mean and medium is greater than 30, if the 
-value is greater than 30 for both then we store it in a 2-d array. 
-We then sort the 2d array by mean and display the location id, average, and median
-'''
-'''
-for x in range(result.size):
-    if result.array[x] > 30:
-        if result_median.array[x] > 30:
-            list.append([result.index[x], result.array[x],result_median.array[x] ])
-list = sorted(list, key=itemgetter(1), reverse=True)
-
-for x in list:
-    print(x)
+result = sorted(result,key=itemgetter(1), reverse=True)
+for x in result:
+    '''maybe scatter plot in here'''
+    result_calculation.append(x[1])
+    result_lat.append(x[2])
+    result_lon.append(x[3])
 
 
-with open('C:\\Users\\Jack\\Desktop\\DataScience\\Ctran\\output.csv', mode='w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(['location_id','average', 'median'])
-    for x in list:
-        writer.writerow(x)
-file.close()
-'''
+t = np.arange(result_lat.__len__())
+plt.scatter(result_lon, result_lat, c=t)
+plt.colorbar();
+plt.show()
+#print (result)
+
